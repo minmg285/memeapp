30250cc905ebebdb04bc154be77512c87091a33c
minmaung285 <mthmaung0001@gmail.com>
2020-07-05 19:04:49 +0630

    Updates

---
+++ precache-manifest.9077d7d89b2a15cb837c41d4aa5c4bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ee6e90639d1cedc901af7ee40842a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998fb58556a1a9eeaffd",
+    "url": "/static/css/main.c6d76286.chunk.css"
+  },
+  {
+    "revision": "5778ffd6bdaf43340576",
+    "url": "/static/js/2.0c6e36e6.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.0c6e36e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998fb58556a1a9eeaffd",
+    "url": "/static/js/main.27cebe89.chunk.js"
+  },
+  {
+    "revision": "a92e8c9db9c5c16741b8",
+    "url": "/static/js/runtime-main.58591f2b.js"
+  },
+  {
+    "revision": "3766780f07e39447b2b46a575b8b817f",
+    "url": "/static/media/bg.3766780f.jpg"
+  }
+]);
